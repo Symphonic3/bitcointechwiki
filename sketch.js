d4ba58ad3202c8161bb278b9a552340d69c8004c
Symphonic <46897919+Symphonic3@users.noreply.github.com>
2023-08-07 14:07:33 -0600

    Add license to sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,22 @@
+/*
+ * bitcointech.wiki transaction editor - a second-generation, visual bitcoin transaction editor
+ * Copyright (C) 2023  Symphonic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 Array.prototype.remove = function() {
 	let pn = false;
 	var what, a = arguments,
